1c8eb7c635924f362ca0d75fd9aca1aa98fb0d63
OmarV26 <vargasomar2601@gmail.com>
2024-10-01 23:22:17 -0300

    add class poleras

--- inventarioApp/models.py
+++ inventarioApp/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Poleras(models.Model):
+    nombre = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    talla = models.CharField(max_length=50)
+    categoria = models.CharField(max_length=100)
+    precio = models.IntegerField()
+    stock = models.IntegerField()
